dbb7ab9f08e0beb3dd4c397171376cf678c3a738
DeVillager <tommi.makelainen@helsinki.fi>
2015-05-12 22:17:49 +0300

    lisätty pulmapelin tiedosto

---
+++ pulmupele/src/main/java/pulmupele/Main.java
@@ -0,0 +1,15 @@
+package pulmupele;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("mitäkä");
+        System.out.print(" ..muuta?");
+
+    }
+}
